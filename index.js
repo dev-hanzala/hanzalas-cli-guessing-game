fb14c581fa45af2d866df4c5afa9cb27a039a51e
dev-hanzala <160335647+dev-hanzala@users.noreply.github.com>
2024-03-23 00:22:25 +0500

    v1.2.0

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 #! /usr/bin/env node
+import inquirer from "inquirer";
 // 1. Computer generates a random number.
-const computerNumber = Math.floor(Math.random());
-console.log(computerNumber);
-export {};
+const computerNumber = Math.floor(Math.random() * 100);
 // 2. Take users number.
-// const userChoice = await inquirer.prompt([
-// 	{
-// 		name: "userNumber",
-// 		type: "number",
-// 		message: "Make a guess: "
-// 	},
-// ]);
-// // 3. Compare.
-// if (userChoice.userNumber === computerNumber) {
-// 	console.log(`Congrats! You guessed correctly.`);
-// } else {
-// 	console.log(`Incorrect guess.`);
-// }
+const userChoice = await inquirer.prompt([
+    {
+        name: "userNumber",
+        type: "number",
+        message: "Make a guess between 0 and 100: "
+    },
+]);
+// 3. Compare.
+if (userChoice.userNumber === computerNumber) {
+    console.log(`Congrats! You guessed correctly.`);
+}
+else {
+    console.log(`Incorrect guess.`);
+}
--- index.ts
+++ index.ts
@@ -4,27 +4,25 @@ import inquirer from "inquirer";
 
 // 1. Computer generates a random number.
 
-const computerNumber = Math.floor(Math.random());
-console.log(computerNumber);
-
+const computerNumber = Math.floor(Math.random() * 100);
 
 // 2. Take users number.
-// const userChoice = await inquirer.prompt([
-// 	{
-// 		name: "userNumber",
-// 		type: "number",
-// 		message: "Make a guess: "
+const userChoice = await inquirer.prompt([
+	{
+		name: "userNumber",
+		type: "number",
+		message: "Make a guess between 0 and 100: "
 
-// 	},
-// ]);
+	},
+]);
 
 
 
-// // 3. Compare.
-// if (userChoice.userNumber === computerNumber) {
-// 	console.log(`Congrats! You guessed correctly.`);
+// 3. Compare.
+if (userChoice.userNumber === computerNumber) {
+	console.log(`Congrats! You guessed correctly.`);
 	
-// } else {
-// 	console.log(`Incorrect guess.`);
+} else {
+	console.log(`Incorrect guess.`);
 	
-// }
+}
