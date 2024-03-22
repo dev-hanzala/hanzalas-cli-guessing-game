5ae4a0404e9ef3bd149b2a191a8eb40ee39801c6
dev-hanzala <160335647+dev-hanzala@users.noreply.github.com>
2024-03-23 00:03:20 +0500

    v1.1.0

--- index.ts
+++ index.ts
@@ -4,25 +4,27 @@ import inquirer from "inquirer";
 
 // 1. Computer generates a random number.
 
-const computerNumber = 45;
+const computerNumber = Math.floor(Math.random());
+console.log(computerNumber);
+
 
 // 2. Take users number.
-const userChoice = await inquirer.prompt([
-	{
-		name: "userNumber",
-		type: "number",
-		message: "Make a guess: "
+// const userChoice = await inquirer.prompt([
+// 	{
+// 		name: "userNumber",
+// 		type: "number",
+// 		message: "Make a guess: "
 
-	},
-]);
+// 	},
+// ]);
 
 
 
-// 3. Compare.
-if (userChoice.userNumber === computerNumber) {
-	console.log(`Congrats! You guessed correctly.`);
+// // 3. Compare.
+// if (userChoice.userNumber === computerNumber) {
+// 	console.log(`Congrats! You guessed correctly.`);
 	
-} else {
-	console.log(`Incorrect guess.`);
+// } else {
+// 	console.log(`Incorrect guess.`);
 	
-}
+// }
